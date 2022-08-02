6477735621753d9fc713a4c79af74d51501f9ba2
Balkundi-Vishwas <95179937+Balkundi-Vishwas@users.noreply.github.com>
2022-08-02 21:19:22 +0530

    2nd commit

--- Admin/admin_login_page.php
+++ Admin/admin_login_page.php
@@ -38,7 +38,7 @@
         ?>
 
         <div class="d-flex justify-content-center align-items-center align-self-center vh-100">
-            <form action="login_page.php" method="post" class="w-25 bg-warning p-4 text-center">
+            <form action="../client/login_page.php" method="post" class="w-25 bg-warning p-4 text-center">
                 <h2>Admin Login</h2>
                 <input type="text" name="uname" placeholder="Enter Username" class="mt-3 form-control">
                 <input type="password" name="password" placeholder="Enter Password" class="mt-3 form-control">      
--- Client/register_page.php
+++ Client/register_page.php
@@ -37,7 +37,7 @@
         <div class="d-flex justify-content-center align-items-center vh-100">
             <form  method="post" class="w-25 bg-warning p-4 text-center">
                 <h2>Register</h2>
-                <input type="text" name="name" placeholder="Enter Name" class="mt-3 form-control">
+                <input type="text" name="name" placeholder="Enter Name" class="mt-3 form-control" required>
                 <input type="text" name="mobile" placeholder="Enter Mobile" maxlength="10" class="mt-3 form-control" required>
                 
                 <input type="password" name="password" placeholder="Enter Password" class="mt-3 form-control" required>
