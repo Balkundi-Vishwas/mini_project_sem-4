0779e479fbd93d30072648a9ff879b48abcd94dc
CHEETAH02 <98352066+CHEETAH02@users.noreply.github.com>
2022-08-03 02:11:58 +0530

    sql tables and queries
    all the sql commands to create tables, indices, etc

---
+++ SQL/dbms_proj.sql
@@ -0,0 +1,161 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 02, 2022 at 10:37 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbms_proj`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `aid` int(11) NOT NULL,
+  `aname` varchar(20) NOT NULL,
+  `uname` varchar(20) NOT NULL,
+  `pass` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `bid` int(10) NOT NULL,
+  `uid` int(10) NOT NULL,
+  `amount` int(10) NOT NULL,
+  `seat` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sport_event`
+--
+
+CREATE TABLE `sport_event` (
+  `sname` varchar(20) NOT NULL,
+  `stime` time NOT NULL,
+  `sdate` date NOT NULL,
+  `ename` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stadium`
+--
+
+CREATE TABLE `stadium` (
+  `sname` varchar(25) NOT NULL,
+  `simage` varchar(25) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `city` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL,
+  `uname` varchar(20) NOT NULL,
+  `upass` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `mobile` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`aid`),
+  ADD UNIQUE KEY `uname` (`uname`);
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`bid`),
+  ADD KEY `uid` (`uid`);
+
+--
+-- Indexes for table `sport_event`
+--
+ALTER TABLE `sport_event`
+  ADD KEY `sname` (`sname`);
+
+--
+-- Indexes for table `stadium`
+--
+ALTER TABLE `stadium`
+  ADD PRIMARY KEY (`sname`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `aid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sport_event`
+--
+ALTER TABLE `sport_event`
+  ADD CONSTRAINT `sport_event_ibfk_1` FOREIGN KEY (`sname`) REFERENCES `stadium` (`sname`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
